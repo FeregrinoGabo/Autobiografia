0bad54411a927480ceff46a80b919298dd77ba77
FeregrinoGabo <you@example.com>
2024-02-10 00:05:44 -0600

    EstiloBibliografia

--- App.js
+++ App.js
@@ -1,38 +1,89 @@
-import {View, Text, StyleSheet, SafeAreaView, ScrollView, Image} from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, Image } from 'react-native';
 import TextComponent from './components/TextComponents';
 
-//Otra forma de importar lo mismo y más, pero es menos limpio
-//import View from 'react-native';
-// import Text from 'react-native'; 
-
 const App = () => {
-  return(
-    <SafeAreaView style = {styles.container}>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.icon}
+          source={require('./assets/images/icon.png')}
+        />
+        <Text style={styles.headerText}>Autobiografía</Text>
+      </View>
       <ScrollView>
-        <Text style = {styles.title}> Mi biografía </Text>
-        <Image style = {styles.image} source={require('./assets/images/rengoku.jpg')} />
-        <TextComponent/>
+        <Text style={styles.title}>Gabriel Feregrino Hernández</Text>
+        <View style={styles.cuerpo}>
+          <Image
+            style={styles.image} 
+            source={require('./assets/images/rengoku.jpg')}
+          />
+        </View>
+        <TextComponent />
+
+        <View style={styles.cuerpo}>
+          <Image
+            style={styles.image} 
+            source={require('./assets/images/rengoku2.jpg')}
+          />
+        </View>
+
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#F2F2F2',
   },
 
-  title:{
-    fontSize: 20,
-    fontWeight: 'bold'
+  header: {
+    backgroundColor: '#C0C0C0',
+    height: 100,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  icon: {
+    width: 50,
+    height: 50,
+    marginTop: 40,
+    marginRight: 20,
+    marginLeft: 10,
+  },
+
+  headerText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 40,
+  },
+
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
   },
 
   image: {
-    width: 100,
-    height: 100
+    width: '100%',
+    height:450,
+    resizeMode: 'contain',
   },
-})
+
+  cuerpo: {
+    height: 450,
+    width: '100%',
+    alignItems: 'center',
+  }
+});
+
+
 
 export default App;
--- components/TextComponents.js
+++ components/TextComponents.js
@@ -1,29 +1,79 @@
-import{Text, View, StyleSheet} from 'react-native';
-
+import { View, Text, StyleSheet } from 'react-native';
 
 const TextComponent = () => {
-    return (
-        <View> 
-          <Text style={styles.text}> Gabriel Feregrino hernández</Text>
-          <Text style={styles.text}> Ocupación: Estudiante </Text>
-          <Text style={styles.text}> Edad: 20 años </Text>
-          <Text style={styles.text}> Fecha de nacimiento: 31/May/2003 </Text>
-          <Text style={styles.text}> Estado civil: Soltero </Text>
-          <Text style={styles.text}> Grado de estudios: Licenciatura en proceso </Text>
-          <Text style={styles.text}> Lugar de nacimiento: Santiego de Querétaro, Querétaro </Text>
-          <Text style={styles.text}> Soy tan solo un instante en el transcurso de vida de toda la existencia, es más
-            mi propia vivencia es insignificante. Nací sin un solo objetivo en la vida. En el todo soy nada y para la nada,
-            soy la nada misma. </Text>
+  return (
+    <View style={styles.container}>
+        <View style={styles.contenedor}>
+            <Text style={styles.titulos}>Datos</Text>
+            <Text style={styles.text}>Gabriel Feregrino Hernández</Text>
+            <Text style={styles.text}>Ocupación: Estudiante</Text>
+            <Text style={styles.text}>Edad: 20 años</Text>
+            <Text style={styles.text}>Fecha de nacimiento: 31/May/2003</Text>
+            <Text style={styles.text}>Estado civil: Soltero</Text>
+            <Text style={styles.text}>Grado de estudios: Licenciatura en proceso</Text>
+            <Text style={styles.text}>Lugar de nacimiento: Santiago de Querétaro, Querétaro</Text>
         </View>
-    );
-}
+
+
+
+      <Text style={styles.titulos}>Vida mía</Text>
+      
+      <Text style={styles.text2}>
+        Soy tan solo un instante en el transcurso de vida de toda la existencia, es más
+        mi propia vivencia es insignificante. Nací sin un solo objetivo en la vida. En el todo soy nada y para la nada,
+        soy la nada misma.
+      </Text>
+
+      <Text style={styles.titulos}>Historia</Text>
+
+      <Text style={styles.text2}>
+        En las misteriosas tierras de Santiago de Querétaro, México, se alzaba la Universidad Autónoma de Querétaro (UAQ), una institución legendaria donde los secretos de la magia se entrelazaban con el conocimiento académico. Entre los muros ancestrales de la UAQ, Gabriel Feregrino Hernández, nacido bajo los auspicios de la luna el 31 de mayo de 2003, emprendió su viaje hacia la grandeza. {'\n'}{'\n'}
+        Desde temprana edad, Gabriel mostró un talento innato para la magia, un don que había heredado de ancestros olvidados en las páginas del tiempo. En la UAQ, bajo la tutela de maestros poderosos y sabios, Gabriel cultivó sus habilidades mágicas, dominando los secretos de los elementos y las artes arcanas.{'\n'}{'\n'}
+        Pero la paz en la UAQ pronto se vio amenazada por las sombras del mal. Una fuerza oscura, conocida como el Culto de la Noche, surgió de las profundidades de la oscuridad, ansiosa por sumir al mundo en un reino de tinieblas. Con su líder, el despiadado mago oscuro Malphas, a la cabeza, el Culto de la Noche sembró el caos y la destrucción por dondequiera que pasaba.{'\n'}{'\n'}
+        En su sexto semestre, Gabriel se encontró en el epicentro de la batalla contra el mal. Con sus amigos y aliados a su lado, se enfrentó al Culto de la Noche en una guerra de magia y espada que sacudió los cimientos del reino. En la batalla final, Gabriel y sus compañeros se encontraron frente a frente con Malphas en un duelo que determinaría el destino del mundo.{'\n'}{'\n'}
+        La lucha fue titánica, con rayos y relámpagos iluminando el cielo oscuro como fuego de dragón. Pero Gabriel no vaciló, con su corazón lleno de determinación y su espada reluciente en la mano. En un acto de pura valentía, enfrentó a Malphas con todo su poder, desatando una ráfaga de magia que consumió el aire a su alrededor.{'\n'}{'\n'}
+        En un instante fugaz, la batalla llegó a su clímax. Gabriel no titubeó, no mostró piedad. Con un movimiento certero, derrotó al malvado mago oscuro, sellando su destino en las sombras de la historia.{'\n'}{'\n'}
+        La UAQ, ahora liberada del yugo del Culto de la Noche, celebró la victoria con júbilo y alegría. Gabriel Feregrino Hernández, el héroe de la UAQ, se convirtió en una leyenda viviente, cuyo nombre sería recordado por generaciones venideras como un símbolo de coraje y esperanza en tiempos de oscuridad. Y así, con su espada en alto y su corazón lleno de determinación, Gabriel se preparó para las muchas aventuras que aún esperaban en su camino, sabiendo que siempre estaría listo para defender la luz contra las sombras del mal.
+      </Text>
+
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 14,
-        fontWeight: 'normal',
-        color: '#487eb0',
-      }
-})
+  container: {
+    flex: 1,
+  },
+
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'normal',
+    color: '#444444',
+    marginBottom: 1.5,
+  },
+
+  text2: {
+    marginLeft: 25,
+    marginRight: 25,
+    marginBottom: 10,
+    marginTop: 5,
+    fontSize: 16,
+    textAlign: 'justify',
+    fontWeight: 'normal',
+    color: '#444444',
+  },
+
+  titulos: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  contenedor: {
+    marginBottom: 40
+  }
+
+});
 
 export default TextComponent;
